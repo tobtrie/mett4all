0dfbab1724824d0a65968d208280703010836458
Tobiass Trienekens <tobias.trienekens@rwth-aachen.de>
2023-04-22 17:24:55 +0200

    fix: remove spotify playlist till 01.05

--- src/scripts/hideComponent.ts
+++ src/scripts/hideComponent.ts
@@ -1,11 +1,14 @@
 import isBefore from "date-fns/isBefore";
 
-const releaseTimestamp = new Date("2023-05-01T22:00:00Z");
+const releaseTimestamp = new Date("2023-04-30T22:00:00Z");
 export function hideComponent(id: string, hideClass: string) {
 	function checkTicketStart() {
 		if (!isBefore(new Date(), releaseTimestamp)) {
-			const ticketSale: HTMLSpanElement | null = document.getElementById(id);
-			ticketSale?.classList?.remove(hideClass);
+			const component: HTMLSpanElement | null = document.getElementById(id);
+			component?.classList?.remove(hideClass);
+			if (component === null) {
+				console.warn("Could not find component with id ", id);
+			}
 			clearInterval(intervalId);
 		}
 	}
